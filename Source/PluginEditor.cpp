b916a583c6ed481d0d3c965992a12f9398bd683f
Benedikt Adams <mail@benediktadams.com>
2024-02-08 11:29:42 -0800

    Added moonbase member

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -15,7 +15,7 @@ MoonbasePluginDemoAudioProcessorEditor::MoonbasePluginDemoAudioProcessorEditor (
 {
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    setSize (800, 600);
 }
 
 MoonbasePluginDemoAudioProcessorEditor::~MoonbasePluginDemoAudioProcessorEditor()
